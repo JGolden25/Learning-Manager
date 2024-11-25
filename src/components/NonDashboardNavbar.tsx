896da25ddad220b95920635ad2cd9fa1bffa5c0f
Joseph Chretien-Golden <josephchretien@Josephs-MBP.attlocal.net>
2024-11-24 22:16:21 -0600

    Landing page added in nondash and animated styling

--- src/components/NonDashboardNavbar.tsx
+++ src/components/NonDashboardNavbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { Bell, BookOpen } from 'lucide-react';
 
 const NonDashboardNavbar = () => {
   return (
@@ -8,8 +9,31 @@ const NonDashboardNavbar = () => {
     <Link href="/" className="nondashboard-navbar__brand">
     JGOLDEN
     </Link>
+    <div className="flex items-center gap-4">
+    <div className="relative group">
+              <Link
+                href="/search"
+                className="nondashboard-navbar__search-input"
+                scroll={false}
+              >
+                <span className="hidden sm:inline">Search Courses</span>
+                <span className="sm:hidden">Search</span>
+              </Link>
+              <BookOpen
+                className="nondashboard-navbar__search-icon"
+                size={18}
+              />
+              </div>
     </div>
-</nav>  )
-}
+    </div>
+    <div className="nondashboard-navbar__actions">
+          <button className="nondashboard-navbar__notification-button">
+            <span className="nondashboard-navbar__notification-indicator"></span>
+            <Bell className="nondashboard-navbar__notification-icon" />
+          </button>
+</div>
+</nav>  
+);
+};
 
 export default NonDashboardNavbar
